895dcbcc1cd8b6168b829fd508865fd452ecc1cd
Jackson Luís Peres das Neves <jackson.luis@jv01.local>
2021-02-03 14:02:34 -0300

    refatored controller and created bind

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,22 +10,31 @@ class NegociacaoController {
             this._negociacoesView.update(model)
         ); */
 
-        this._listaNegociacoes = ProxyFactory.create(
-            new ListaNegociacoes(),
-            ['adiciona', 'esvazia'], (model) => {
-                this._negociacoesView.update(model);
-            }
-        );
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
-        this._negociacoesView.update(this._listaNegociacoes);
-        
-        this._mensagem = ProxyFactory.create(
-            new Mensagem(), ['texto'], (model) => {
-                this._mensagemView.update(model)
-            }
+        this._listaNegociacoes =  new Bind(
+            new ListaNegociacoes(),
+            this._negociacoesView,
+            ['adiciona', 'esvazia']
         );
+        // this._listaNegociacoes = ProxyFactory.create(
+            // new ListaNegociacoes(),
+            // ['adiciona', 'esvazia'], (model) => {
+                // this._negociacoesView.update(model);
+            // }
+        // );
+        
         this._mensagemView = new MensagemView($('#mensagemView'));
-        this._mensagemView.update(this._mensagem)
+        this._mensagem =  new Bind(
+            new Mensagem(),
+            this._mensagemView,
+            ['textp']
+        );
+        // this._mensagem = ProxyFactory.create(
+            // new Mensagem(), ['texto'], (model) => {
+                // this._mensagemView.update(model)
+            // }
+        // );
+        
     }
 
     adiciona(event) {
